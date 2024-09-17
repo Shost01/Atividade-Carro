942897571154f7afe919b693d739e90b235db428
Nathan Henrique <ribeiro.nathan2002@gmail.com>
2024-09-17 18:16:56 -0300

    1 versão Atividade carro

---
+++ Atvdd_carro/Carro.cs
@@ -0,0 +1,47 @@
+﻿class Carro
+{
+    private int ano;
+    public string Fabricante { get; set; }
+    public string Modelo { get; set; }
+    public int Ano
+    {
+        get => ano;
+        set
+        { 
+            if(value <1960 || value > 2023)
+            {
+                Console.WriteLine("Valor inválido, insira um ano entre 1960 e 2023");
+            }
+            else
+            {
+                ano = value;
+            }
+        }
+    }
+    public int Qtdportas { get; set; }
+    public int velocidade = 0;
+    public string DescricaoDetalhada => $"Informações:\nFabricante = {Fabricante}\nModelo = {Modelo}\nAno = {ano}\nPortas = {Qtdportas}";
+
+
+    public void Acelerar()
+    {
+        Console.WriteLine("Vrummmm.....");
+        if ( velocidade < 100 ) 
+        {
+            velocidade = velocidade + 5;
+        }
+    }
+    public void Frear()
+    {
+        Console.WriteLine("Freando....");
+        if( velocidade > 0 )
+        {
+            velocidade = velocidade - 5;    
+        }
+    }
+
+    public void Buzinar() 
+    {
+        Console.WriteLine("Bi Bi Biiiiiiiii");
+    }
+}
---
+++ Atvdd_carro/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Carro carro = new Carro();
+carro.Fabricante = "Wolkswagen";
+carro.Modelo = "Gol";
+carro.Ano = 2024;
+carro.Qtdportas = 4;
+
+Console.WriteLine(carro.DescricaoDetalhada); 
+carro.Acelerar();
+carro.Frear();
+carro.Buzinar();*/
+
+Produto produto = new Produto();
+produto.Marca = "Ypê";
+produto.Nome = "Detergente";
+produto.Preco = -1;
+produto.Estoque = -1;
+
+Console.WriteLine(produto.DescricaoDetalhadaP);
